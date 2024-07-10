45181469b4f73eb6e9bc5d511ecea8012fc334e7
Darshan.Bhut <Darshan.Bhut@icc.exela.in>
2024-07-10 18:37:45 +0530

    UserModel updated

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
